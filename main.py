99acc1f491b149ab829839d330887cfe85ea3e20
Ваше Имя <z7758@mail.ru>
2022-07-06 19:25:24 +0300

    magical methods

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-class Car:
-    wheels_number = 4
+class Chain:
+    def __init__(self, number_of_items):
+        self.number_of_items = number_of_items
 
-    def __init__(self, name, color, year, is_crashed):
-        self.name = name
-        self.color = color
-        self.year = year
-        self.is_crashed = is_crashed
+    def __str__(self):
+        return 'Chain with ' + str(self.number_of_items) + ' items'
 
-    def drive(self, city, km):
-        print(self.name + ' Car is driving ' + city + ' ' + km)
-
-    def chang_color(self, new_color):
-        self.color = new_color
+    def __len__(self):
+        return self.number_of_items
 
 
-opel = Car('Opel', 'red', 1999, True)
-opel.drive('Borisov', '15')
-bmw = Car('bmw', 'black', 2003, False )
-bmw.drive('Zhodino', '3')
-bmw.chang_color('yellow')
-print(bmw.color)
+jake = Chain(20)
+
+print(jake)
+
+print(len(jake))
+
+
